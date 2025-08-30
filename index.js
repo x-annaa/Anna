e89e3712e79dc8fae97c15fb6570c45d5aaeaec7
x-annaa <annaannamx@gmail.com>
2025-08-30 08:36:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// ⚡ 初始化 Supabase
-const SUPABASE_URL = "https://ffdrwsemmfvqlqhyjlnb.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZHJ3c2VtbWZ2cWxxaHlqbG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDI1ODQsImV4cCI6MjA3MTg3ODU4NH0.x7TQHZ2af8O_f9ye__mT6eVstlH9BiyVkNVaOnL3h74";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// ❌ 不需要再写 Supabase 初始化了，直接用 supabaseClient
+// const SUPABASE_URL = "...";
+// const SUPABASE_KEY = "...";
+// const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // 切换窗口
 document.getElementById("showLogin").addEventListener("click", () => {
