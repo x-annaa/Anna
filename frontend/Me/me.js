b5b7647dc6e101e045ced55f0e81e40163b531cd
x-annaa <annaannamx@gmail.com>
2025-09-03 07:07:42 -0700

    Update me.js

--- frontend/Me/me.js
+++ frontend/Me/me.js
@@ -4,23 +4,6 @@
 let currentUser = null;
 
 // ======================
-// 页面切换
-// ======================
-const buttons = document.querySelectorAll(".bottom-nav button");
-const pages = document.querySelectorAll(".page");
-
-buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    buttons.forEach(b => b.classList.remove("active"));
-    btn.classList.add("active");
-
-    const pageId = btn.dataset.page;
-    pages.forEach(p => p.classList.remove("active"));
-    document.getElementById(pageId).classList.add("active");
-  });
-});
-
-// ======================
 // 加载用户信息
 // ======================
 async function loadUserInfo(username) {
