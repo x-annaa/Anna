d1473eec45f1fbd1b49de341f2022f567ec6565b
x-annaa <annaannamx@gmail.com>
2025-09-03 00:29:56 -0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,3 +1,5 @@
+//  数据库  //
+
 window.supabaseClient = window.supabase.createClient(
   "https://ffdrwsemmfvqlqhyjlnb.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZHJ3c2VtbWZ2cWxxaHlqbG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDI1ODQsImV4cCI6MjA3MTg3ODU4NH0.x7TQHZ2af8O_f9ye__mT6eVstlH9BiyVkNVaOnL3h74"
