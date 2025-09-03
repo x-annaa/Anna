489d5031ec4efbbfdf4b201c29589ce5c22ee0b4
x-annaa <annaannamx@gmail.com>
2025-09-03 06:43:54 -0700

    Update supabaseClient.js

--- frontend/supabaseClient.js
+++ frontend/supabaseClient.js
@@ -1,4 +1,4 @@
-// ⚡ Supabase 初始化 (全局共用)
+// ⚡ Supabase 初始化 (全局共用) supabaseClient.js
 const SUPABASE_URL = "https://ffdrwsemmfvqlqhyjlnb.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZHJ3c2VtbWZ2cWxxaHlqbG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDI1ODQsImV4cCI6MjA3MTg3ODU4NH0.x7TQHZ2af8O_f9ye__mT6eVstlH9BiyVkNVaOnL3h74";
 
