20896459d23b8eb636d65c729d0c53525494e8e7
Unine <annaannamx@gmail.com>
2025-09-22 12:15:23 -0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 //  数据库  //
 
 window.supabaseClient = window.supabase.createClient(
-  "https://ofaxbeydyeajdgwwqrzz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9mYXhiZXlkeWVhamRnd3dxcnp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0NzQxMTQsImV4cCI6MjA3NDA1MDExNH0.mJGdh6BBEy2Mp83H7aBEo3wIFyIsUsVfqgTErgsvFdY"
+  "https://jetkopbzwgqjjefilyrw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpldGtvcGJ6d2dxamplZmlseXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NjY3NTQsImV4cCI6MjA3NDE0Mjc1NH0.vUO63EX-Pyb3hWmI0GY2pfoeGD6mZZaz3n0pxYB_yU8"
 );
