39abe644ef2fab187c24859744bdb39f32fe5ce5
Unine <annaannamx@gmail.com>
2025-09-24 01:29:56 -0700

    Update supabaseClient.js

--- frontend/Order/supabaseClient.js
+++ frontend/Order/supabaseClient.js
@@ -1,5 +1,7 @@
 // ⚡ Supabase 初始化 (全局共用) supabaseClient.js
-const SUPABASE_URL = "https://ffdrwsemmfvqlqhyjlnb.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZHJ3c2VtbWZ2cWxxaHlqbG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzMDI1ODQsImV4cCI6MjA3MTg3ODU4NH0.x7TQHZ2af8O_f9ye__mT6eVstlH9BiyVkNVaOnL3h74";
+const SUPABASE_URL = "https://airkbwolmkidaokqhxjj.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcmtid29sbWtpZGFva3FoeGpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2NDU0NzIsImV4cCI6MjA3NDIyMTQ3Mn0.vs0jWR6_FEeEhZ8h7-WruTyjWmxm2qmN5b-hzjvw2zQ";
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+
