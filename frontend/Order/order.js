c001845c6d2ee60beaedc7872d67fd115ca966a7
Unine <annaannamx@gmail.com>
2025-09-27 14:34:14 -0700

    Update order.js

--- frontend/Order/order.js
+++ frontend/Order/order.js
@@ -682,7 +682,7 @@ async function loadLastOrder() {
 
   const { data: orders } = await supabaseClient
     .from("orders")
-    .select(`id, total_price, profit, status, created_at, products ( name, profit )`)
+    .select(`id, total_price, profit, status, created_at, products ( name, profit, url )`)
     .eq("user_id", window.currentUserId)
     .order("created_at", { ascending: false })
     .limit(1);
