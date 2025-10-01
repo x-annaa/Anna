f3bcb8d712bace7ba64849a8032bee027bdf4f53
Unine <annaannamx@gmail.com>
2025-10-01 06:42:25 -0700

    Update order.js

--- frontend/Order/order.js
+++ frontend/Order/order.js
@@ -739,7 +739,7 @@ function setMatchingState(isMatching) {
 
   if (btn) {
     btn.disabled = isMatching;
-    btn.textContent = isMatching ? "matching..." : "Order";
+    btn.textContent = isMatching ? "wait" : "Order";
   }
 }
 
