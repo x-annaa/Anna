04c1affbe8792183b5c3a46fab2b0144442badc1
Unine <annaannamx@gmail.com>
2025-10-02 08:20:34 -0700

    Update order.js

--- frontend/Order/order.js
+++ frontend/Order/order.js
@@ -754,12 +754,12 @@ document.getElementById("autoOrderBtn").addEventListener("click", function() {
 /* 规则弹窗 */
 const rulesBtn = document.getElementById('Rules');
 const popup = document.getElementById('rulesPopup');
-const backBtn = document.getElementById('backBtn1');
+const backBtn = document.getElementById('backBtn'); // 只声明一次
 
 rulesBtn.addEventListener('click', () => {
-  popup.style.display = 'flex'; // 显示弹窗
+  popup.style.display = 'flex';
 });
 
 backBtn.addEventListener('click', () => {
-  popup.style.display = 'none'; // 关闭弹窗
+  popup.style.display = 'none';
 });
