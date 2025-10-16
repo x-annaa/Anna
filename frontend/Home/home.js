fe3152bcedd7bdb91a2aae8063f1834e9bce53ee
Unine <annaannamx@gmail.com>
2025-10-16 12:36:14 -0700

    Update home.js

--- frontend/Home/home.js
+++ frontend/Home/home.js
@@ -45,7 +45,7 @@ window.logout = async function () {
 
 const adUrls = [
   "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping.jpg",
-  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/SALE.jpg",
+  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/SEALU9.jpg",
   "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/SALE.jpg"
 ];
 
