70964f4ad64c92c29b2cc14687a32308142865f6
Unine <annaannamx@gmail.com>
2025-10-19 14:43:24 -0700

    Update home.js

--- frontend/Home/home.js
+++ frontend/Home/home.js
@@ -46,8 +46,7 @@ window.logout = async function () {
 const adUrls = [
   "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/U9shopping.jpg",
   "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/SEALU9.jpg",
-  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/421u.jpg",
-  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/usa.jpg"
+  "https://uiwttxqdoplttyrfqcbb.supabase.co/storage/v1/object/public/Photos/Phones/USAUK.jpg"
 ];
 
 let currentAdIndex = 0;
