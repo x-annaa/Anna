b885513a868e265f45887de9d03f3d86b825befe
Unine <annaannamx@gmail.com>
2025-10-26 13:06:08 -0700

    Update order.js

--- frontend/Order/order.js
+++ frontend/Order/order.js
@@ -778,7 +778,7 @@ document.querySelector(".left-box").addEventListener("click", async () => {
         return `
           <li>
             ${img}
-            <div style="font-weight:600;">
+            <div style="font-weight:700; font-size:14px;">
               ${o.products?.name || '未知商品'}
             </div>
             <div style="text-align:left;">
