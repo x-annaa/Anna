6666ffda1a926e1c75496891d003663349f50677
Unine <annaannamx@gmail.com>
2025-10-26 13:27:35 -0700

    Update order.js

--- frontend/Order/order.js
+++ frontend/Order/order.js
@@ -787,7 +787,7 @@ document.querySelector(".left-box").addEventListener("click", async () => {
               描述：${o.products?.description || '暂无描述'}
             </div>
             <div>价格：¥${Number(o.total_price).toFixed(2)}</div>
-            <div>利润率：${Number(o.products?.profit || Done)}</div>
+            <div>利润率：${Number(o.products?.profit || 0)}</div>
             <div>收入：+¥${Number(o.profit).toFixed(2)}</div>
             <div>状态：${o.status === "completed" ? "✅ 已完成" : "⏳ 待完成"}</div>
             ${time}
